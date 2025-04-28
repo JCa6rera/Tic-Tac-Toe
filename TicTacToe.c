771056e14fd38c0730370673f7f33cd72f424659
JCa6rera <cabrerajc2004@gmail.com>
2025-04-28 14:20:42 -0700

    menu improvements

--- TicTacToe.c
+++ TicTacToe.c
@@ -2,6 +2,7 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 const char PLAYER1 = 'X';
 const char PLAYER2 = 'O';
@@ -12,6 +13,9 @@ char board[3][3];
 void playerVSplayerFlow();
 void resetBoard();
 void printBoard();
+char checkWinner();
+bool askToPlayAgain();
+void printError();
 
 int main(){
     //starting menu for the game
@@ -20,19 +24,20 @@ int main(){
     
 
     printf("Hello! Welcome to my Tic-Tac-Toe game!\n");
-    int choice;
-    char confirmation;
+    int choice = 0;
+    char confirmation = ' ';
     bool condition = true;
     while(condition){
-        choice = 0;
         while(choice != 1 && choice != 2){
             printf("How would you like to play?\n1. Player vs Player\n2. Player vs Computer\n");
+
             if(scanf("%d", &choice) != 1){
                 printf("Invalid input! Please enter 1 or 2.\n");
                 while(getchar() != '\n');
                 choice = 0;
                 continue;
             }
+
             switch (choice){
                 case 1:
                     printf("You chose Player vs Player! Let's Begin\n");
@@ -46,20 +51,19 @@ int main(){
                     printf("Wrong input! Please input a number 1 or 2\n");
                     break;
             }
+
         }
 
         if(WINNER == 'X' || WINNER == 'O'){
-            printf("%c has won the game! Would you like to play again?(Y/N)\n", WINNER);
-            scanf(" %c", &confirmation);
-            confirmation = toupper(confirmation);
-            if(confirmation == 'N'){
+            printf("%c has won the game!\n", WINNER);
+            if(!askToPlayAgain()){
+                printf("Goodbye!\n");
                 condition = false;
             }
         } else {
-            printf("Looks like nobody one. Would you like to play again?(Y/N)\n");
-            scanf(" %c", &confirmation);
-            confirmation = toupper(confirmation);
-            if(confirmation == 'N'){
+            printf("Looks like nobody one!\n");
+            if(!askToPlayAgain()){
+                printf("Goodbye!\n");
                 condition = false;
             }
         }
@@ -67,8 +71,44 @@ int main(){
     return 0;
 }
 
+void printError(){
+    printf("Invalid input! Please enter Y or N\n");
+}
+
+bool askToPlayAgain(){
+    char input[10];
+    char confirmation = ' ';
+    while(confirmation != 'Y' && confirmation != 'N'){
+        printf("Would you like to play again?(Y/N)?\n");
+        if(fgets(input, sizeof(input), stdin) == NULL){
+            printError();
+            continue;
+        }
+
+        //remove newline char
+        size_t len = strlen(input);
+        if(len > 0 && input[len - 1] == '\n'){
+            input[len - 1] = '\0';
+        }
+
+        if(strlen(input) == 1){
+            confirmation = toupper(input[0]);
+            if(confirmation != 'Y' && confirmation != 'N'){
+                printError();
+            }
+        } else {
+            printError();
+        }
+    }
+
+    return confirmation == 'Y';
+}
+
+char checkWinner(){
+    
+}
+
 void playerVSplayerFlow(){
-    printBoard();
 }
 
 void resetBoard(){
